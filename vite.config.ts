3af3e46300daa76394da2b795f0678121d664bff
Adam Gąsowski <adam.gasowski@gander.pl>
2022-11-04 00:24:59 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import monkey, { cdn } from 'vite-plugin-monkey';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        namespace: 'npm/vite-plugin-monkey',
+        match: ['https://skyrimcommands.com/perks'],
+      },
+      build: {
+        externalGlobals: {
+          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+        },
+      },
+    }),
+  ],
+});
