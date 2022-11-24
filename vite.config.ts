eb61fcb6361ada53574870017dce1f7471cae6c3
Adam Gąsowski <adam.gasowski@gander.pl>
2022-11-24 11:44:22 +0100

    add meta and version update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import monkey, { cdn } from 'vite-plugin-monkey';
+import {version} from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,13 +12,15 @@ export default defineConfig({
       userscript: {
         namespace: 'npm/vite-plugin-monkey',
         match: ['https://skyrimcommands.com/perks'],
-        version: '0.1.0',
+        version,
         author: 'Adam Gąsowski',
+        updateURL: 'https://github.com/gander/monkey-skyrimcommands/releases/latest/download/monkey-skyrimcommands.user.js'
       },
       build: {
         externalGlobals: {
           vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
         },
+        metaFileName: true
       },
     }),
   ],
